34deaabf0cfebf35876483d79219f02a10eeba32
Martin <34376785+G4Cab@users.noreply.github.com>
2023-04-10 17:18:53 +0200

    Add files via upload

---
+++ bambukiller.bat
@@ -0,0 +1,6 @@
+del /F /S /Q %APPDATA%\BambuStudio
+del /F /S /Q %APPDATA%\BambuStudio-Softfever
+del /F /S /Q %APPDATA%\OrcaSlicer
+del /F /S /Q %LOCALAPPDATA%\BambuStudio
+del /F /S /Q %LOCALAPPDATA%\BambuStudio-Softfever
+del /F /S /Q %LOCALAPPDATA%\OrcaSlicer
