faeb2836897d9d452ad9ec2560e101f9454a6083
Martin <34376785+G4Cab@users.noreply.github.com>
2023-12-18 10:15:00 +0100

    Update bambuslicer-profilekiller.bat
    - changed the bavior from deleting the files to moving them into backup folders
    
    - added BambuStudioBeta folders

--- bambuslicer-profilekiller.bat
+++ bambuslicer-profilekiller.bat
@@ -1,17 +1,31 @@
 @echo off
 setlocal
 :PROMPT
-echo "This script will delete your BambuSlicer, BambuSlicer-SoftFever and OrcaSlicer local profiles"
-echo "These are located under %APPDATA% and %LOCALAPPDATA%"
+echo This script will move your BambuSlicer, BambuSlicer-SoftFever and OrcaSlicer local profiles
+echo These are located under %APPDATA% and %LOCALAPPDATA%
+echo .
 SET /P AREYOUSURE=Are you sure to continue (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
-del /F /S /Q %APPDATA%\BambuStudio
-del /F /S /Q %APPDATA%\BambuStudio-Softfever
-del /F /S /Q %APPDATA%\OrcaSlicer
-del /F /S /Q %LOCALAPPDATA%\BambuStudio
-del /F /S /Q %LOCALAPPDATA%\BambuStudio-Softfever
-del /F /S /Q %LOCALAPPDATA%\OrcaSlicer
+  echo .
+  SET /P AREYOUSUREBAMBU=Do you want to move BambuStudio Profile directory (Y/[N])?
+  IF /I "%AREYOUSUREBAMBU%" NEQ "Y" GOTO END
+    move /Y %APPDATA%\BambuStudio %APPDATA%\BambuStudio_backup
+    move /Y %LOCALAPPDATA%\BambuStudio %LOCALAPPDATA%\BambuStudio_backup
+    move /Y %APPDATA%\BambuStudioBeta %APPDATA%\BambuStudioBeta_backup
+    move /Y %LOCALAPPDATA%\BambuStudioBeta %LOCALAPPDATA%\BambuStudioBeta_backup
+  :END
+
+  echo .
+  SET /P AREYOUSUREORCA=Do you want to move OrcaSlicer Profile directory (Y/[N])?
+  IF /I "%AREYOUSUREORCA%" NEQ "Y" GOTO END
+    move /Y %APPDATA%\BambuStudio-Softfever %APPDATA%\BambuStudio-Softfever_backup
+    move /Y %APPDATA%\OrcaSlicer %APPDATA%\OrcaSlicer_backup
+    move /Y %LOCALAPPDATA%\BambuStudio-Softfever %LOCALAPPDATA%\BambuStudio-Softfever_backup
+    move /Y %LOCALAPPDATA%\OrcaSlicer %LOCALAPPDATA%\OrcaSlicer_backup
+  :END
 
 :END
 endlocal
+echo .
+pause
